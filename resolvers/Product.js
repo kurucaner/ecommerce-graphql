4da90cbc768399787625335b968821d56d458024
Caner <kurucaner@gmail.com>
2022-02-02 21:00:14 -0500

    Text content params

---
+++ resolvers/Product.js
@@ -0,0 +1,5 @@
+exports.Product = {
+  category: ({ categoryId }, args, { categories }) => {
+    return categories.find((category) => category.id === categoryId);
+  },
+};
---
+++ resolvers/Query.js
@@ -0,0 +1,15 @@
+exports.Query = {
+  hello: () => {
+    return "World!";
+  },
+  products: (parent, args, { products }) => products,
+
+  product: (parent, { id }, { products }) => {
+    return products.find((product) => product.id === id);
+  },
+  categories: (parent, args, { categories }) => categories,
+
+  category: (parent, { id }, { categories }) => {
+    return categories.find((category) => category.id === id);
+  },
+};
